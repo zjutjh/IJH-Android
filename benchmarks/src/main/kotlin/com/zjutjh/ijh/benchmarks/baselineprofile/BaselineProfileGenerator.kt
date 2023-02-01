8e07e77d4a163857ad5de1c7bfadf9719fe1b90a
I-Info <info@i1nfo.com>
2023-02-02 00:15:15 +0800

    perf: update baseline profile

--- benchmarks/src/main/kotlin/com/zjutjh/ijh/benchmarks/baselineprofile/BaselineProfileGenerator.kt
+++ benchmarks/src/main/kotlin/com/zjutjh/ijh/benchmarks/baselineprofile/BaselineProfileGenerator.kt
@@ -3,8 +3,6 @@ package com.zjutjh.ijh.benchmarks.baselineprofile
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.uiautomator.By
-import androidx.test.uiautomator.Direction
-import androidx.test.uiautomator.Until
 import com.zjutjh.ijh.benchmarks.PACKAGE_NAME
 import org.junit.Rule
 import org.junit.Test
@@ -21,16 +19,10 @@ class BaselineProfileGenerator {
             pressHome()
             startActivityAndWait()
             with(device) {
-                wait(Until.findObject(By.scrollable(true)), 1_000)
-                findObject(By.scrollable(true)).also {
-                    it.setGestureMargin(device.displayWidth / 10)
-                    it.scroll(Direction.DOWN, 0.6f)
-                    it.scroll(Direction.UP, 0.8f)
-                }
                 findObject(By.desc("Navigation")).click()
+                waitForIdle()
                 findObject(By.desc("Collapse")).click()
 
-                findObject(By.desc("Login"))?.click()
                 waitForIdle()
                 pressBack()
             }
